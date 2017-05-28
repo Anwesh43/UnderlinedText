e2ff8e2ddcb4559844cd2dff7f7e8f83e587c7c4
Anwesh43 <anweshthecool0@gmail.com>
2017-05-29 03:54:07 +0530

    using layout in list

--- underlinedtext/src/main/java/com/anwesome/ui/underlinedtext/UnderlinedTextList.java
+++ underlinedtext/src/main/java/com/anwesome/ui/underlinedtext/UnderlinedTextList.java
@@ -1,6 +1,7 @@
 package com.anwesome.ui.underlinedtext;
 
 import android.app.Activity;
+import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 
 /**
@@ -11,19 +12,23 @@ public class UnderlinedTextList {
     private Activity activity;
     private HorizontalScrollView horizontalScrollView;
     private boolean isShown = false;
+    private UnderlineTextLayout underlineTextLayout;
     public UnderlinedTextList(Activity activity) {
         this.activity = activity;
         horizontalScrollView = new HorizontalScrollView(activity);
+        underlineTextLayout = new UnderlineTextLayout(activity);
+        horizontalScrollView.addView(underlineTextLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
     public void show() {
         if(!isShown) {
+            underlineTextLayout.addIndicator();
             activity.setContentView(horizontalScrollView);
             isShown = true;
         }
     }
     public void addText(String text) {
         if(!isShown) {
-
+            underlineTextLayout.addText(text);
         }
     }
 }
