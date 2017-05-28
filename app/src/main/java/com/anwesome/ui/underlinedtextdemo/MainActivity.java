025d49ece5f19b3fdc0d65e4230130b187bdc724
Anwesh43 <anweshthecool0@gmail.com>
2017-05-29 04:29:33 +0530

    using UnderlineText in app

--- app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
@@ -3,11 +3,18 @@ package com.anwesome.ui.underlinedtextdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anwesome.ui.underlinedtext.UnderlinedTextList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String text = "Malcolm James McCormick, best known by his stage name Mac Miller, is an American rapper";
+        UnderlinedTextList underlinedTextList = new UnderlinedTextList(this);
+        for(String token:text.split(" ")) {
+            underlinedTextList.addText(token);
+        }
+        underlinedTextList.show();
     }
 }
