f572b152251a12033f3a295524173c22c0bd7409
Anwesh43 <anweshthecool0@gmail.com>
2017-05-29 03:52:35 +0530

    adding text and indicator

--- underlinedtext/src/main/java/com/anwesome/ui/underlinedtext/UnderlineTextLayout.java
+++ underlinedtext/src/main/java/com/anwesome/ui/underlinedtext/UnderlineTextLayout.java
@@ -41,28 +41,39 @@ public class UnderlineTextLayout extends ViewGroup{
         int viewW = w/20,viewH = 0;
         for(int i=0;i<getChildCount();i++) {
             View child = getChildAt(i);
-            measureChild(child,wspec,hspec);
-            viewW += (child.getMeasuredWidth()+w/20);
-            viewH = Math.max(viewH,child.getMeasuredHeight());
-
-
+            if(child instanceof UnderlinedTextView) {
+                measureChild(child, wspec, hspec);
+                viewW += (child.getMeasuredWidth() + w / 20);
+                viewH = Math.max(viewH, child.getMeasuredHeight());
+            }
         }
         setMeasuredDimension(viewW,w/20+viewH*2);
     }
     public void addIndicator() {
         indicator = new IndicatorView(getContext());
-        addView(indicator,new LayoutParams(w/20,h/60));
+        if(getChildCount() > 0) {
+            UnderlinedTextView firstTextView = (UnderlinedTextView) getChildAt(0);
+            indicator.initX(firstTextView.getX()+firstTextView.getMeasuredWidth()/2-w/30);
+        }
+        addView(indicator,new LayoutParams(w/15,h/60));
         requestLayout();
     }
     public void addText(String text) {
-
+        UnderlinedTextView underlinedTextView = new UnderlinedTextView(getContext(),text);
+        addView(underlinedTextView,new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+        requestLayout();
     }
     public void onLayout(boolean reloaded,int a,int b,int w,int h) {
         int x = w/20,y = w/20;
         for(int i=0;i<getChildCount();i++) {
             View child = getChildAt(i);
-            child.layout(x,y,x+child.getMeasuredWidth(),y+child.getMeasuredHeight());
-            x += (child.getMeasuredWidth()+w/20);
+            if(child instanceof UnderlinedTextView) {
+                child.layout(x, y, x + child.getMeasuredWidth(), y + child.getMeasuredHeight());
+                x += (child.getMeasuredWidth() + w / 20);
+            }
+            else if(child instanceof IndicatorView){
+                child.layout((int)child.getX(),y+h/15,(int)child.getX()+child.getMeasuredWidth(),y+h/15+child.getMeasuredHeight());
+            }
         }
     }
     private class UnderlinedTextView extends AppCompatTextView {
