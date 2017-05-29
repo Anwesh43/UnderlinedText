825f4999a2824c2191e20e3bd2ee0bcd7a5e3f47
Anwesh43 <anweshthecool0@gmail.com>
2017-05-29 15:13:41 +0530

    making screen landscape

--- app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anwesome.ui.underlinedtextdemo;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         String text = "Malcolm James McCormick, best known by his stage name Mac Miller, is an American rapper";
         UnderlinedTextList underlinedTextList = new UnderlinedTextList(this);
         for(String token:text.split(" ")) {
