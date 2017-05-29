a5fe662c8290f170d05cb7fa1ac979fed8da0c3e
Anwesh43 <anweshthecool0@gmail.com>
2017-05-29 15:08:57 +0530

    Using OnClickListener in app

--- app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/underlinedtextdemo/MainActivity.java
@@ -2,7 +2,9 @@ package com.anwesome.ui.underlinedtextdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.anwesome.ui.underlinedtext.OnClickListener;
 import com.anwesome.ui.underlinedtext.UnderlinedTextList;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,13 @@ public class MainActivity extends AppCompatActivity {
         String text = "Malcolm James McCormick, best known by his stage name Mac Miller, is an American rapper";
         UnderlinedTextList underlinedTextList = new UnderlinedTextList(this);
         for(String token:text.split(" ")) {
-            underlinedTextList.addText(token);
+            final String currToken = token;
+            underlinedTextList.addText(token, new OnClickListener() {
+                @Override
+                public void onClick() {
+                    Toast.makeText(MainActivity.this, String.format("%s clicked",currToken), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
         underlinedTextList.show();
     }
